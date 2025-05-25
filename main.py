f7d1577eb3469be0c9f856291b11725668f33243
211293104 <doraemon.211293104@gmail.com>
2025-05-25 23:53:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,35 @@ import os
 
 app = Flask(__name__)
 
-LINE_CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
-LINE_CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
+# 環境変数からトークンとシークレットを取得
+LINE_CHANNEL_ACCESS_TOKEN = os.environ['CHANNEL_ACCESS_TOKEN']
+LINE_CHANNEL_SECRET = os.environ['CHANNEL_SECRET']
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
+@app.route("/", methods=['GET'])
+def index():
+    return "Hello, this is the LINE bot server!", 200
+
 @app.route("/callback", methods=['POST'])
 def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
     return 'OK', 200
 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
+
+if __name__ == "__main__":
+    app.run()
